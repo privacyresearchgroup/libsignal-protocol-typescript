737e3a9dbaf339b7dbc3d2e2e20b2ab7856f594c
Rolfe Schmidt <rolfeschmidt@gmail.com>
2020-07-01 16:24:25 -0500

    move stuff to async from .then

--- src/fingerprint-generator.ts
+++ src/fingerprint-generator.ts
@@ -1,67 +1,65 @@
 import { FingerprintGeneratorType } from './'
 import ByteBuffer from 'bytebuffer'
-var msrcrypto = require('../lib/msrcrypto')
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const msrcrypto = require('../lib/msrcrypto')
 
 export class FingerprintGenerator implements FingerprintGeneratorType {
     VERSION = 0
 
-    createFor(
+    async createFor(
         localIdentifier: string,
         localIdentityKey: ArrayBuffer,
         remoteIdentifier: string,
         remoteIdentityKey: ArrayBuffer
     ): Promise<string> {
-        return Promise.all([
-            this.getDisplayStringFor(localIdentifier, localIdentityKey, this._iterations),
-            this.getDisplayStringFor(remoteIdentifier, remoteIdentityKey, this._iterations),
-        ]).then(function (fingerprints) {
-            return fingerprints.sort().join('')
-        })
+        const localStr = await getDisplayStringFor(localIdentifier, localIdentityKey, this._iterations)
+        const remoteStr = await getDisplayStringFor(remoteIdentifier, remoteIdentityKey, this._iterations)
+        return [localStr, remoteStr].sort().join('')
     }
 
     private _iterations: number
     constructor(_iterations: number) {
         this._iterations = _iterations
     }
+}
 
-    async iterateHash(data: ArrayBuffer, key: ArrayBuffer, count: number) {
-        const data1 = ByteBuffer.concat([data, key]).toArrayBuffer()
-        let result = await msrcrypto.subtle.digest({ name: 'SHA-512' }, data1)
+async function getDisplayStringFor(identifier: string, key: ArrayBuffer, iterations: number): Promise<string> {
+    const bb = ByteBuffer.concat([Uint8Array.from([0, 0]), key, identifier])
+    const bytes = bb.toArrayBuffer()
+    const hash = await iterateHash(bytes, key, iterations)
+    const output = new Uint8Array(hash)
+    return (
+        getEncodedChunk(output, 0) +
+        getEncodedChunk(output, 5) +
+        getEncodedChunk(output, 10) +
+        getEncodedChunk(output, 15) +
+        getEncodedChunk(output, 20) +
+        getEncodedChunk(output, 25)
+    )
+}
 
-        if (--count === 0) {
-            return result
-        } else {
-            let r1 = new Uint8Array(result)
-            return this.iterateHash(r1, key, count)
-        }
-    }
+async function iterateHash(data: ArrayBuffer, key: ArrayBuffer, count: number): Promise<ArrayBuffer> {
+    const data1 = ByteBuffer.concat([data, key]).toArrayBuffer()
+    const result = (await msrcrypto.subtle.digest({ name: 'SHA-512' }, data1)) as ArrayBuffer
 
-    getDisplayStringFor(identifier: string, key: ArrayBuffer, iterations: number) {
-        const bb = ByteBuffer.concat([Uint8Array.from([0, 0]), key, identifier])
-        const bytes = bb.toArrayBuffer()
-        return this.iterateHash(bytes, key, iterations).then(function (output) {
-            output = new Uint8Array(output)
-            return (
-                getEncodedChunk(output, 0) +
-                getEncodedChunk(output, 5) +
-                getEncodedChunk(output, 10) +
-                getEncodedChunk(output, 15) +
-                getEncodedChunk(output, 20) +
-                getEncodedChunk(output, 25)
-            )
-        })
+    if (--count === 0) {
+        return result
+    } else {
+        const r1 = new Uint8Array(result)
+        return iterateHash(r1, key, count)
     }
 }
 
 function getEncodedChunk(hash, offset): string {
-    var chunk =
+    const chunk =
         (hash[offset] * Math.pow(2, 32) +
             hash[offset + 1] * Math.pow(2, 24) +
             hash[offset + 2] * Math.pow(2, 16) +
             hash[offset + 3] * Math.pow(2, 8) +
             hash[offset + 4]) %
         100000
-    var s = chunk.toString()
+    let s = chunk.toString()
     while (s.length < 5) {
         s = '0' + s
     }
