6bc605dbbea9ad05e540a1eb9ab4bed747cb60df
Rolfe Schmidt <rolfeschmidt@gmail.com>
2020-07-01 18:14:59 -0500

    add ECC functions to internal crypto

--- src/internal/crypto.ts
+++ src/internal/crypto.ts
@@ -1,4 +1,13 @@
+import * as Internal from '.'
+import { KeyPairType } from '../types'
+
 export class Crypto {
+    private _curve: Internal.AsyncCurve
+
+    constructor() {
+        this._curve = new Internal.AsyncCurve()
+    }
+
     static getRandomBytes(n: number): ArrayBuffer {
         // TODO: A real implementation with WebCrypto!!!
         const bytes = Array(n)
@@ -6,5 +15,24 @@ export class Crypto {
             bytes[i] = Math.floor(256 * Math.random())
         }
         return Uint8Array.from(bytes).buffer.slice(0)
+    }
+
+    createKeyPair(privKey: ArrayBuffer | undefined): Promise<KeyPairType> {
+        if (privKey === undefined) {
+            privKey = Crypto.getRandomBytes(32)
+        }
+        return this._curve.createKeyPair(privKey)
+    }
+
+    ECDHE(pubKey: ArrayBuffer, privKey: ArrayBuffer): Promise<ArrayBuffer> {
+        return this._curve.ECDHE(pubKey, privKey)
+    }
+
+    Ed25519Sign(privKey: ArrayBuffer, message: ArrayBuffer): Promise<ArrayBuffer> {
+        return this._curve.Ed25519Sign(privKey, message)
+    }
+
+    Ed25519Verify(pubKey: ArrayBuffer, msg: ArrayBuffer, sig: ArrayBuffer): Promise<boolean> {
+        return this._curve.Ed25519Verify(pubKey, msg, sig)
     }
 }
