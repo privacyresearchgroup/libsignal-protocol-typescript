38b1d07dfa6a2b23dca623ff9bd6d7c9fe74b929
Rolfe Schmidt <rolfeschmidt@gmail.com>
2020-07-01 18:03:44 -0500

    add published curve functions

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
+import { Curve25519Wrapper } from '@rolfe/curve25519-typescript'
+import { Curve } from './curve'
+
 export * from './types'
+export * from './signal-protocol-address'
+
+import * as Internal from './internal'
+
+// returns a promise of something with the shape of the old libsignal
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+export default async () => {
+    const cw = await Curve25519Wrapper.create()
+
+    return {
+        Curve: new Curve(new Internal.Curve(cw)),
+    }
+}
