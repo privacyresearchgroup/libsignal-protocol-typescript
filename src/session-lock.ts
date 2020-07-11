6db4421201933d3f1f56da82e0154dbb3ef4b088
Rolfe Schmidt <rolfeschmidt@gmail.com>
2020-07-11 06:42:53 -0500

    Catche errors sent to floating promise

--- src/session-lock.ts
+++ src/session-lock.ts
@@ -1,21 +1,29 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /*
  * jobQueue manages multiple queues indexed by device to serialize
  * session io ops on the database.
  */
 
-let jobQueue = {}
+const jobQueue: { [k: string]: Promise<any> } = {}
 
 export type JobType = () => Promise<any>
 
 export class SessionLock {
     static queueJobForNumber(id: string, runJob: JobType): Promise<any> {
+        console.log(`queue job`, { id, jobQueue })
         const runPrevious = jobQueue[id] || Promise.resolve()
         const runCurrent = (jobQueue[id] = runPrevious.then(runJob, runJob))
-        runCurrent.then(function () {
-            if (jobQueue[id] === runCurrent) {
-                delete jobQueue[id]
-            }
-        })
+        runCurrent
+            .then(function () {
+                console.log(`clear queue`, { id, jobQueue })
+                if (jobQueue[id] === runCurrent) {
+                    delete jobQueue[id]
+                }
+            })
+            .catch((e) => {
+                console.warn(e)
+            })
+        console.log(`returning runCurrent`, { id, jobQueue })
         return runCurrent
     }
 }
