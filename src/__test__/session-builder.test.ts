7af213c939abea2cfe8a6220f6e10167dd11bf22
Rolfe Schmidt <rolfeschmidt@gmail.com>
2020-07-14 14:27:45 -0500

    test the cipher

--- src/__test__/session-builder.test.ts
+++ src/__test__/session-builder.test.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 import { SessionBuilder } from '../session-builder'
-//SESSIONCIPHER import { SessionCipher } from '../session-cipher'
+import { SessionCipher } from '../session-cipher'
 import { SessionRecord } from '../session-record'
 
 import { SignalProtocolAddress } from '../signal-protocol-address'
@@ -9,7 +9,6 @@ import { SignalProtocolStore } from './storage-type'
 import { generateIdentity, generatePreKeyBundle, assertEqualArrayBuffers } from '../__test-utils__/utils'
 import * as utils from '../helpers'
 import { KeyHelper } from '../key-helper'
-import { SessionCipher } from '../session-cipher'
 
 jest.setTimeout(30000)
 
@@ -74,14 +73,12 @@ describe('basic prekey v3', function () {
         assertEqualArrayBuffers(plaintext, originalMessage) // assertEqualArrayBuffers(plaintext, originalMessage)
     })
 
-    /*SESSIONCIPHER
-test('basic prekey v3: the session can decrypt', async () => {
-    await prep
-    const ciphertext = await bobSessionCipher.encrypt(originalMessage)
-    const plaintext = await aliceSessionCipher.decryptWhisperMessage(ciphertext.body, 'binary')
-    assertEqualArrayBuffers(plaintext, originalMessage)
-})
-*/
+    test('basic prekey v3: the session can decrypt', async () => {
+        // await prep
+        const ciphertext = await bobSessionCipher.encrypt(originalMessage)
+        const plaintext = await aliceSessionCipher.decryptWhisperMessage(ciphertext.body!, 'binary')
+        assertEqualArrayBuffers(plaintext, originalMessage)
+    })
 
     test('basic prekey v3: accepts a new preKey with the same identity', async () => {
         // await prep
@@ -148,26 +145,22 @@ describe('basic v3 NO PREKEY', function () {
         expect(sessionRecord.getOpenSession()).toBeDefined()
     })
 
-    /*SESSIONCIPHER
-test('basic v3 NO PREKEY: the session can encrypt', async () => {
-    //await prep
-    const ciphertext = await aliceSessionCipher.encrypt(originalMessage)
-    expect(ciphertext.type).toBe(3) // PREKEY_BUNDLE
+    test('basic v3 NO PREKEY: the session can encrypt', async () => {
+        //await prep
+        const ciphertext = await aliceSessionCipher.encrypt(originalMessage)
+        expect(ciphertext.type).toBe(3) // PREKEY_BUNDLE
 
-    const plaintext = await bobSessionCipher.decryptPreKeyWhisperMessage(ciphertext.body, 'binary')
+        const plaintext = await bobSessionCipher.decryptPreKeyWhisperMessage(ciphertext.body!, 'binary')
 
-    assertEqualArrayBuffers(plaintext, originalMessage)
-})
-*/
+        assertEqualArrayBuffers(plaintext, originalMessage)
+    })
 
-    /*SESSIONCIPHER
-test('basic v3 NO PREKEY: the session can decrypt', async () => {
-   // await prep
-    const ciphertext = await bobSessionCipher.encrypt(originalMessage)
-    const plaintext = await aliceSessionCipher.decryptWhisperMessage(ciphertext.body, 'binary')
-    assertEqualArrayBuffers(plaintext, originalMessage)
-})
-*/
+    test('basic v3 NO PREKEY: the session can decrypt', async () => {
+        // await prep
+        const ciphertext = await bobSessionCipher.encrypt(originalMessage)
+        const plaintext = await aliceSessionCipher.decryptWhisperMessage(ciphertext.body!, 'binary')
+        assertEqualArrayBuffers(plaintext, originalMessage)
+    })
 
     // test('basic v3 NO PREKEY: accepts a new preKey with the same identity', async () => {
     //     //await prep
