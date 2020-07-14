69181914bdb412bec6a749fdade87f79e59eaa1c
Mark Johnson <maejohns@gmail.com>
2020-07-14 13:36:16 -0700

    add describe and clean to make consistent with our other testing style, and do simple regex

--- src/__test__/session-lock.test.ts
+++ src/__test__/session-lock.test.ts
@@ -3,68 +3,70 @@ import { SessionLock } from '../session-lock'
 async function sleep(ms: number) {
     return new Promise((resolve) => setTimeout(resolve, ms))
 }
+describe('session-lock', function () {
+    test('return something', async () => {
+        let value = ''
+        await Promise.all([
+            SessionLock.queueJobForNumber('channel1', async () => {
+                await sleep(10)
+                value += 'xyz'
+                return Promise.resolve()
+            }),
+        ])
 
-test('return something', async () => {
-    let value = ''
-    await Promise.all([
-        SessionLock.queueJobForNumber('channel1', async () => {
-            await sleep(10)
-            value += 'xyz'
-            return Promise.resolve()
-        }),
-    ])
+        expect(value).toBe('xyz')
+    })
 
-    expect(value).toBe('xyz')
-})
+    test('return longshort', async () => {
+        let value = ''
+        await Promise.all([
+            SessionLock.queueJobForNumber('channel1', async () => {
+                await sleep(3000)
+                value += 'long'
+            }),
+            SessionLock.queueJobForNumber('channel1', async () => {
+                await sleep(1)
+                value += 'short'
+            }),
+        ])
 
-test('return longshort', async () => {
-    let value = ''
-    await Promise.all([
-        SessionLock.queueJobForNumber('channel1', async () => {
-            await sleep(3000)
-            value += 'long'
-        }),
-        SessionLock.queueJobForNumber('channel1', async () => {
-            await sleep(1)
-            value += 'short'
-        }),
-    ])
+        expect(value).toBe('longshort')
+    })
 
-    expect(value).toBe('longshort')
-})
+    test('return shortlong', async () => {
+        let value = ''
+        await Promise.all([
+            SessionLock.queueJobForNumber('channel1', async () => {
+                await sleep(1)
+                value += 'short'
+            }),
+            SessionLock.queueJobForNumber('channel1', async () => {
+                await sleep(2000)
+                value += 'long'
+            }),
+        ])
 
-test('return shortlong', async () => {
-    let value = ''
-    await Promise.all([
-        SessionLock.queueJobForNumber('channel1', async () => {
-            await sleep(1)
-            value += 'short'
-        }),
-        SessionLock.queueJobForNumber('channel1', async () => {
-            await sleep(2000)
-            value += 'long'
-        }),
-    ])
+        expect(value).toBe('shortlong')
+    })
 
-    expect(value).toBe('shortlong')
-})
-
-test('multichannel', async () => {
-    let value = ''
-    await Promise.all([
-        SessionLock.queueJobForNumber('channel1', async () => {
-            await sleep(4000)
-            value += 'long'
-        }),
-        SessionLock.queueJobForNumber('channel2', async () => {
-            await sleep(1)
-            value += 'ch2'
-        }),
-        SessionLock.queueJobForNumber('channel1', async () => {
-            await sleep(1)
-            value += 'short'
-        }),
-    ])
-
-    expect(value).toBe('ch2longshort')
+    test('multichannel', async () => {
+        let value = ''
+        await Promise.all([
+            SessionLock.queueJobForNumber('channel1', async () => {
+                await sleep(4000)
+                value += 'long'
+            }),
+            SessionLock.queueJobForNumber('channel2', async () => {
+                await sleep(1)
+                value += 'ch2'
+            }),
+            SessionLock.queueJobForNumber('channel1', async () => {
+                await sleep(1)
+                value += 'short'
+            }),
+        ])
+        const re = /ch2/gi
+        const newstr = value.replace(re, '')
+        expect(newstr).toBe('longshort')
+    })
 })
