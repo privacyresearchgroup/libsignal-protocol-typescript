9a90dd735eaae05bc44142740a53c9be3677c019
Rolfe Schmidt <rolfeschmidt@gmail.com>
2020-07-17 12:19:08 -0500

    allow client to set webcrypto and curve

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,8 @@ export * from './curve'
 
 import * as Internal from './internal'
 
+export { setWebCrypto, setCurve } from './internal'
+
 // returns a promise of something with the shape of the old libsignal
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export default async () => {
