3c248142a02112bc3b7fdf3399607c1822d43f1b
Rolfe Schmidt <rolfeschmidt@gmail.com>
2020-07-20 09:08:34 -0500

    Clear queue and handle errors in SessionLock

--- src/session-lock.ts
+++ src/session-lock.ts
@@ -9,18 +9,28 @@ const jobQueue: { [k: string]: Promise<any> } = {}
 export type JobType<T> = () => Promise<T>
 
 export class SessionLock {
+    static errors: any[] = []
+    static _promises: Promise<any>[] = []
     static queueJobForNumber<T>(id: string, runJob: JobType<T>): Promise<T> {
         const runPrevious = jobQueue[id] || Promise.resolve()
         const runCurrent = (jobQueue[id] = runPrevious.then(runJob, runJob))
-        runCurrent
+        const promise = runCurrent
             .then(function () {
                 if (jobQueue[id] === runCurrent) {
                     delete jobQueue[id]
                 }
             })
-            .catch((_) => {
-                // console.warn(e)
+            .catch((e) => {
+                // SessionLock callers should already have seen these errors on their own
+                // Promise chains, but we need to handle them here too so we just save them
+                // so callers can review them.
+                SessionLock.errors.push(e)
             })
+        SessionLock._promises.push(promise)
         return runCurrent
+    }
+
+    static async clearQueue(): Promise<void> {
+        await Promise.all(SessionLock._promises)
     }
 }
